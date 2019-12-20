9e935f35921039d288b2bc9ba43617bfd0d35866
icditwang <icditwang@163.com>
2019-12-20 17:34:48 +0800

    upload files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery'
+import Axios from 'axios'
+const axios = Axios.create();
+class App extends React.Component{
+    login(){
+        axios.request({
+            url:"login.php",
+            method:'get',
+            params:{
+                username:$("#uname").val(),
+                pass:$("#userpwd").val()
+            }
+        }).then(res=>{
+            alert('login success');
+        })
+    }
+    render() {
+            return (
+                <form action="" method="post">
+                <table>
+                <tr>
+                <td align="right">用户名：</td>
+            <td><input type="text" name="username" value="user" id="uname"/></td>
+                </tr>
+                <tr>
+                <td align="right">密 码：</td>
+            <td><input type="password" id="userpwd" /></td>
+                </tr>
+                <tr>
+                <td><input type="submit" name="submit" value="登陆" onClick={this.login}/></td>
+            <td><a href="register.php">注册</a></td>
+            </tr>
+            </table>
+            </form>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        );
+    }
+
+
 }
 
 export default App;
